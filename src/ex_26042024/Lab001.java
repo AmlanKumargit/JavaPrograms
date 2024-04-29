947cd5100d49a0291de96858712ac629799d4e5f
Amlan Kumar <amlantest23@example.com>
2024-04-29 11:54:23 +0530

    28th April 2024

---
+++ src/ex_26042024/Lab001.java
@@ -0,0 +1,10 @@
+package ex_26042024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        int age=20;
+        age=-1;
+        System.out.println(age);
+    }
+}
